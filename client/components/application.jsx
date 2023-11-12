c2f95238db68005e6f72bbc171092a2c7163d2c6
BorseHaraldsen <Augustborse@gmail.com>
2023-11-12 15:17:17 +0100

    Not functioning loginform

--- client/components/application.jsx
+++ client/components/application.jsx
@@ -1,6 +1,44 @@
 import { useEffect, useState } from "react";
 
 export function Application() {
+  const [username, setUsername] = useState();
+
+  const [credentials, setCredentials] = useState();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    const res = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ credentials }),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      throw new Error("Failed to log in " + res.statusText);
+    }
+  }
+
+  if (!username) {
+    return (
+      <>
+        <form onSumbit={handleLogin}>
+          Username:
+          <input
+            type="text"
+            value={credentials}
+            onChange={setCredentials(e.target.value)}
+          />
+          <button>Log In.</button>
+        </form>
+      </>
+    );
+  }
+
+  return <ChatWindow />;
+}
+
+export function ChatWindow() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [webSocket, setWebSocket] = useState();
