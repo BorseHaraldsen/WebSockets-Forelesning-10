1d92a5b5f7437703db47ec8e5c0c473aded2e65b
BorseHaraldsen <Augustborse@gmail.com>
2023-11-12 14:19:31 +0100

    Added useState for the list instead

--- client/components/application.jsx
+++ client/components/application.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export function Application() {
+  const [messages, setMessages] = useState(["hello", "Everybody! : )"]);
+
   return (
     <>
       <header>
@@ -6,86 +10,13 @@ export function Application() {
       </header>
 
       <main>
-        <li>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, vel?
-        </li>
-        <li>
-          Amet, aperiam consequatur consequuntur dolore dolorem eveniet facilis
-          magnam quod.
-        </li>
-        <li>
-          Atque culpa error laborum nemo nostrum possimus reiciendis sint
-          veritatis.
-        </li>
-        <li>
-          Accusamus dolore eos in libero molestias officiis praesentium,
-          repellat velit!
-        </li>
-        <li>
-          Deleniti dolorem excepturi facere illo nemo nostrum officiis quasi,
-          voluptatem.
-        </li>
-        <li>
-          Commodi consequuntur fugiat illo in nemo pariatur praesentium
-          quisquam, voluptate.
-        </li>
-        <li>
-          Ad aperiam consectetur corporis distinctio esse laudantium placeat
-          quod tenetur!
-        </li>
-        <li>
-          Debitis dolor eaque molestiae molestias pariatur quidem rerum soluta
-          sunt?
-        </li>
-        <li>
-          A aspernatur corporis debitis eligendi error, odio tenetur voluptatem
-          voluptatibus!
-        </li>
-        <li>
-          Assumenda, hic, sapiente. Incidunt minima nulla odio reprehenderit
-          saepe sequi.
-        </li>
-        <li>
-          Asperiores cum dolorem doloremque enim expedita possimus provident
-          quia reprehenderit.
-        </li>
-        <li>
-          Accusantium asperiores beatae dolor ducimus eaque, impedit ipsum iste
-          nobis?
-        </li>
-        <li>
-          Accusamus aliquid ea, ex neque sequi veritatis voluptatum! Earum,
-          officia!
-        </li>
-        <li>
-          Accusantium corporis cupiditate dolor laudantium possimus. Aut
-          blanditiis tenetur veniam.
-        </li>
-        <li>
-          Alias aliquam eligendi fugit illo omnis quisquam sequi tenetur
-          veritatis.
-        </li>
-        <li>
-          Amet asperiores autem dignissimos earum, incidunt quas ratione sint
-          voluptate.
-        </li>
-        <li>
-          Autem distinctio ea fugiat incidunt nemo, sit. Eos, id temporibus?
-        </li>
-        <li>
-          Aliquam architecto at aut doloremque iste minima quis suscipit ut.
-        </li>
-        <li>
-          Aut qui quo unde. Architecto consectetur fugit inventore ipsum minima!
-        </li>
-        <li>
-          Accusantium aspernatur commodi consectetur eveniet laudantium
-          molestias quaerat vel. Dicta!
-        </li>
+        {messages.map((message) => (
+          <li>{message}</li>
+        ))}
       </main>
 
       <footer>
-        <input />
+        <input autoFocus />
         <button>Chat</button>
       </footer>
     </>
